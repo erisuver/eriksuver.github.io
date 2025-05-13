6dd47be16690c7293bcf275bbf5afda3e83b0a76
eris <sukwibu@gmail.com>
2025-05-13 16:30:17 +0700

    fix eror

--- js/app.js
+++ js/app.js
@@ -1,41 +1,56 @@
 jQuery(document).foundation();
 
-
 jQuery(document).ready(function($) {
-
-        var dataPorto = [
+    var dataPorto = [
         {
-            'image': [
+            'images': [
                 '../images/project1-img1.png',
                 '../images/project1-img2.png',
                 '../images/project1-img3.png'
             ],
-            'url': 'https://github.com/erisuver/pos-siomay-apps'
+            'url': 'https://github.com/erisuver/pos-siomay-apps/'
         },
         {
-            'image': [
+            'images': [
                 '../images/project2-img1.png',
                 '../images/project2-img2.png',
                 '../images/project2-img3.png'
             ],
-            'url': 'https://github.com/erisuver/toolshop-apps'
+            'url': 'https://github.com/erisuver/toolshop-apps/'
         },
         {
-            'image': [
+            'images': [
                 '../images/project3-img1.png',
                 '../images/project3-img2.png',
                 '../images/project3-img3.png'
             ],
             'url': 'https://pasienqu.com/'
         }
-        // Tambahkan proyek lainnya di sini
+        // Add more projects as needed
     ];
 
-    $portoContainer = $('#porto-container');
-    dataPorto.forEach(e => {
-        let $item = $('<div class="cell medium-4"></div>');
-        $item.html('<div class="item-porto"><a href="'+e.url+'" target="_blank"><img src="'+e.image+'" class="image"></a></div>');
-        $item.appendTo($portoContainer);
-    });
+    var $portoContainer = $('#porto-container');
 
-})
+    // Loop through the portfolio data and generate items
+    dataPorto.forEach(function(project) {
+        var $projectItem = $('<div class="cell medium-4"></div>'); // Each project container
+        var $link = $('<a href="' + project.url + '" target="_blank" class="project-link"></a>'); // GitHub link
+
+        var $imagesContainer = $('<div class="project-images"></div>'); // Container for images
+
+        // Loop through images for each project
+        project.images.forEach(function(imagePath) {
+            var $image = $('<img src="' + imagePath + '" class="image" alt="Project Image">');
+            $imagesContainer.append($image);
+        });
+
+        // Add images container inside the link
+        $link.append($imagesContainer);
+
+        // Append the link inside the project item
+        $projectItem.append($link);
+
+        // Finally, append the project item to the main portfolio container
+        $portoContainer.append($projectItem);
+    });
+});
